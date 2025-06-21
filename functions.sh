013850b5e89ce5d5c5cf6231980667e0a7d0ed1d
Faizan960 <faizanpatel2226@gmail.com>
2025-06-21 20:56:11 +0530

    Clean up: Removed duplicate scripts from root folder

--- functions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-greet() {
-           echo "Hello $1!!!!!!!!!! Welcome to DevOps!!!"
-}
-
-greet "Faizan
-" 
-
--- until-loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-n=1
-
-until [ $n -gt 5 ]
-do
-  echo "until loop: $n"
-  n=$((n+1))
-done
